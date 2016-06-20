dd02d7b35bc2ff0b249b26246ed62cde1027c9f1
Carlo Sivilotti <carlo@sivilotti.com>
2016-06-20 16:17:48 -0400

    GUI Draft

--- test_proj/IPsamp/urls.py
+++ test_proj/IPsamp/urls.py
@@ -6,4 +6,5 @@ from IPsamp import views
 
 urlpatterns = [
 	url(r'^$', views.index, name= 'index1'),
+	url(r'^samp/', views.sample, name =  'sample'),
 ]
--- test_proj/IPsamp/views.py
+++ test_proj/IPsamp/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 def index(request):
     return render(request,"IPsamp/index.html")
 
+def sample(request):
+    return render(request,"IPsamp/01.html")
+
