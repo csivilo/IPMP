ba9d1923b48ac32d8f4fb847469f8f4a1dacf95f
Dean Helwig <helwde01@gettysburg.edu>
2016-07-08 15:27:24 -0400

    add row button

--- test_proj/welcome/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from django.conf.urls import include
-from django.contrib import admin
-from welcome import views
-
-
-urlpatterns = [
-	url(r'^$', views.index, name= 'welcome'),
-]
--- test_proj/welcome/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-def index(request):
-    return render(request, 'welcome/welcome.html')
-
-# Create your views here.
